6714b58cba571da407ad9e3ccf8d8a3c1a78c20d
Aaron Latham <alatham7891@gmail.com>
2022-10-05 00:14:07 +0100

    added try and catch for errors

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const button = document.querySelector("button");
 const jokeText = document.querySelector("p");
 
 button.addEventListener("click", async () => {
-  const config = { headers: { Accept: "application/json" } };
-  const req = await axios.get("https://icanhazdadjoke.com/", config);
-  jokeText.textContent = req.data.joke;
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const req = await axios.get("https://icanhazdadjoke.com/", config);
+    jokeText.textContent = req.data.joke;
+  } catch {
+    jokeText.textContent =
+      "Sorry, there was a problem with the jokes API, try again later.";
+  }
 });
 
 (async () => {
